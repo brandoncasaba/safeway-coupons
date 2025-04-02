c155768329ab0cf9b72bf9bb69b9beb46b6ac60e
Brandon Chalk <brandon@casaba.com>
2025-04-02 09:57:45 -0700

    Update chrome_driver.py to add version detection
    Prevent version mismatch on chrome driver by detecting installed version

--- safeway_coupons/chrome_driver.py
+++ safeway_coupons/chrome_driver.py
@@ -1,6 +1,7 @@
 import contextlib
 import subprocess
 import sys
+import re
 from pathlib import Path
 from collections.abc import Iterator
 
@@ -18,6 +19,8 @@ CHROMEDRIVER_PATH = (
 class ChromeDriverDoesNotExist(Exception):
     pass
 
+class ChromeDoesNotExist(Exception):
+    pass
 
 @contextlib.contextmanager
 def chrome_driver(headless: bool = True) -> Iterator[uc.Chrome]:
@@ -35,10 +38,22 @@ def chrome_driver(headless: bool = True) -> Iterator[uc.Chrome]:
         options.add_argument(option)
     if headless:
         options.add_argument("--headless=new")
-    driver = uc.Chrome(options=options)
+    version_main = re.findall('(\\d+)\\.', chrome_version())
+    if len(version_main) > 0:
+        version_main = version_main[0]
+    driver = uc.Chrome(options=options, version_main=int(version_main))
     yield driver
     driver.quit()
 
+def chrome_version() -> str:
+    chrome_path = Path('/usr/bin/google-chrome')
+    if not chrome_path.is_file():
+        raise ChromeDoesNotExist(
+            f"Error: {chrome_path} does not exist"
+        )
+    cmd = [str(chrome_path), "--version"]
+    result = subprocess.run(cmd, capture_output=True)
+    return result.stdout.decode()
 
 def chrome_driver_version() -> str:
     if not CHROMEDRIVER_PATH.is_file():
@@ -50,11 +65,10 @@ def chrome_driver_version() -> str:
     result = subprocess.run(cmd, capture_output=True)
     return result.stdout.decode()
 
-
 def init() -> None:
-    with contextlib.suppress(ChromeDriverDoesNotExist):
-        print(chrome_driver_version())
-        return
+#    with contextlib.suppress(ChromeDriverDoesNotExist):
+#        print(chrome_driver_version())
+#        return
     print("Initializing Chrome Driver")
     with chrome_driver() as driver:
         print("Connect to example.com")
